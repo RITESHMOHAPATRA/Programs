4c6dd5ea646ac4660bba2f6ce6f22458165aedda
sarthak567 <samantaray.subhasarthak@gmail.com>
2017-05-18 22:18:41 +0530

    Add files via upload

---
+++ gemstone.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <set>
+using namespace std;
+
+int main() {
+  int n,x = 10000;
+  cout << "\n Enter the number of strings:";
+  cin >> n;
+  vector<char> vec;
+
+  int pos = 0;
+  for(int i=0;i<n;i++){
+    set<char> ste;
+    string s;
+    cin >> s;
+    for(int j=0;j<s.length();j++){
+        ste.insert(s[j]);
+    }
+
+    int t = ste.size();
+    if(t < x)
+      x = t;
+    vec.insert(vec.begin() + pos,ste.begin(),ste.end());
+    pos += t;
+  }
+
+  cout << "Vector elements:";
+  for(auto i: vec)
+    cout << i << "\t";
+
+  sort(vec.begin(),vec.end());
+  int counter = 0;
+  int j = 1;
+  for(int i=0;i<vec.size();i = i+j){
+    j = 1;
+    while(vec[i] == vec[i+j]){
+      j++;
+    }
+    if(n == j)
+      counter++;
+  }
+
+  cout << "\n Common elements:" << counter << endl;
+  return 0;
+}
